12dc5a775179f3dac368e605485d6128dbb23759
Luciana Miki Ochiai <Luciana.Ochiai@ibm.com>
2023-07-31 19:47:57 -0300

    adding redirect

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react'
 import '../assets/styles/home.css';
 import HomeButton from './homeButton';
 import SocialButtonn from './socialButtons';
 
 function Home() {
   return (
-    <body>
+    <div>
         <HomeButton/>
         <SocialButtonn/>
-    </body>
+    </div>
   );
 }
 
