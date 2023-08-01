0a76b293c3a9099af28a6a34f68e7c8fcfb08690
Luciana Miki Ochiai <Luciana.Ochiai@ibm.com>
2023-08-01 11:34:05 -0300

    adding home text

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import '../assets/styles/home.css';
 import HomeButton from './homeButton';
 import SocialButtonn from './socialButtons';
+import HomeText from './homeText';
 
 function Home() {
   return (
     <div>
         <HomeButton/>
         <SocialButtonn/>
+        <HomeText/>
     </div>
   );
 }
---
+++ src/components/homeText.jsx
@@ -0,0 +1,24 @@
+import "../assets/styles/homeText.css";
+
+function HomeText() {
+
+  return (
+    <div className="text-container">
+        <h1 className="title">
+            Welcome.
+        </h1>
+        <div className="spacer"></div>
+        <div className="p-container">
+            <div>My name is Luciana Miki Ochiai</div>
+            <div>I'm a front-end developer based in Mogi das Cruzes, São Paulo, Brazil</div>
+            <div>Graduated in System Analysis and Development by Fatec Mogi das Cruzes,</div>
+            <div>I've worked to IBM as a application developer, building web and mobile applicaitons.</div>
+            <br/>
+            <div>I'm passionate about user experience, agile methodologies and development.</div>
+        </div>
+        
+    </div>
+  );
+}
+
+export default HomeText;
