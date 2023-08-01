56f719be8106278aae2e8a07f9b0298f22b42b05
Luciana Miki Ochiai <Luciana.Ochiai@ibm.com>
2023-08-01 10:38:50 -0300

    adding links

--- src/components/socialButtons.jsx
+++ src/components/socialButtons.jsx
@@ -1,4 +1,8 @@
 import "../assets/styles/socialButtons.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function SocialButtons() {
 
@@ -6,11 +10,9 @@ function SocialButtons() {
     <div>
         <div className="container">
             <ul>
-                <li><a><i className="fa fa-arrows" aria-hidden="true"></i></a></li>
-                <li><a><i className="fa fa-btc" aria-hidden="true"></i></a></li>
-                <li><a><i className="fa fa-globe" aria-hidden="true"></i></a></li>
-                <li><a><i className="fa fa-home" aria-hidden="true"></i></a></li>
-                <li><a><i className="fa fa-repeat" aria-hidden="true"></i></a></li>
+                <li><a href="mailto:lucianamikiochiai@gmail.com"><i className="fa fa-arrows" aria-hidden="true"><FontAwesomeIcon icon={faEnvelope} /></i></a></li>
+                <li><a href="https://github.com/LucianaMiki"><i className="fa fa-btc" aria-hidden="true"><FontAwesomeIcon icon={faGithub} /></i></a></li>
+                <li><a href="https://www.linkedin.com/in/lucianamikiochiai/"><i className="fa fa-globe" aria-hidden="true"><FontAwesomeIcon icon={faLinkedin} /></i></a></li>
             </ul>
         </div>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './components/home';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Home />
